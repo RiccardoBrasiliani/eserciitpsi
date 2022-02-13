ccaaac9150a25fd408234d937c41d95573ced1c2
RiccardoBrasiliani <94352050+RiccardoBrasiliani@users.noreply.github.com>
2022-02-13 10:28:09 +0100

    aggiunto esercizioclasse3

---
+++ esercizioclasse3.c
@@ -0,0 +1,30 @@
+#include printf("%d\n",1);<stdioh>
+int main(int argc, char*argv[])
+{
+int n1,n2;
+double ncompresi;
+
+printf("inserisci il valore di n1 \n");
+scanf("%d",&n1);
+printf("inserisci il volore di n2\n");
+scanff ("%d",&n2);
+while (n1<0 && n2<0)
+{
+printf("inserisci il valore positivo\n");
+scanf ("%d",&n1);
+scanf("%d",&n2);
+}
+while(n2<n1)
+{
+printf("inserisci il valore di n1 \n");
+scanf("%d",&n1);
+printf("inserisci il volore di n2\n");
+scanff ("%d",&n2);
+
+}
+for (int i=0;i<=n2; i++);
+{
+printf("%d\n",i);
+}
+return=0;
+}
