168706b70103dfd3c9d5f4c74774d464b5deb1d9
RiccardoBrasiliani <94352050+RiccardoBrasiliani@users.noreply.github.com>
2022-02-13 10:39:23 +0100

    aggiunto eserciziocasa

---
+++ eserciziocasa.c
@@ -0,0 +1,20 @@
+#include <stdioh>
+int main(int argc, char*argv[])
+{
+int n;
+double -n;
+
+printf("inserisci il valore\n");
+scanf("%d",&n);
+
+    while (n<0)
+     {
+      printf("inserisci il valore\n");
+      scanf("%d",&n);
+     }
+    for (int i=0;i>=-n && i<=n;i++)
+     {
+      printf("%d\n",i);
+     }
+return=0;
+}
