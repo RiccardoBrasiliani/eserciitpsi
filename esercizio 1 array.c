95a45d8ae56030e1c5f5db47699959a7095296de
RiccardoBrasiliani <94352050+RiccardoBrasiliani@users.noreply.github.com>
2022-03-13 21:45:39 +0100

    commit finale

---
+++ esercizio 1 array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ( int argc, char*argv [])
+{
+    int popolami[n];
+    int n=4;
+    int i;
+
+    for(i=0; i<4;i++)
+    {
+        printf("inserisci un numero\n");
+        scanf("%d", & popolami[i]); 
+        printf(popalami[i]);
+    }
+    
+    return=0
+}
