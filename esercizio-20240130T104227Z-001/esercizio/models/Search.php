bd0d621acd28a0c64198bae66854ad3defb7d295
RiccardoBrasiliani <94352050+RiccardoBrasiliani@users.noreply.github.com>
2024-01-31 10:28:08 +0100

    Delete esercizio-20240130T104227Z-001/esercizio directory

--- esercizio-20240130T104227Z-001/esercizio/models/Search.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "database.php";
-require_once "parola.php";
-$parola = $_GET["q"];
-$parole = parola::GetParole($parola);
-foreach ($parole as $p) {
-    echo "<li>" . $p["parola"] . "</li>";
-}
